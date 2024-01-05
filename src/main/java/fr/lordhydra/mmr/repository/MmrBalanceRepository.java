b72f8b5230503212bdf1dcd2eecdb4072b34bd86
Vincent <vincent.choquert0@gmail.com>
2024-01-05 13:17:42 +0100

    feat: save mmr balance

--- src/main/java/fr/lordhydra/mmr/repository/MmrBalanceRepository.java
+++ src/main/java/fr/lordhydra/mmr/repository/MmrBalanceRepository.java
@@ -1,11 +1,13 @@
 package fr.lordhydra.mmr.repository;
 
+import fr.lordhydra.mmr.entities.MmrBalanceEntity;
 import fr.lordhydra.mmr.services.StorageService;
 import fr.lordhydra.mmr.utils.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 public class MmrBalanceRepository implements Repository {
 
@@ -25,6 +27,35 @@ public class MmrBalanceRepository implements Repository {
                 """;
         try {
             PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            Logger.getInstance().error(e.getMessage());
+        }
+    }
+
+    public void insert(MmrBalanceEntity mmrBalanceEntity) {
+        Connection connection = StorageService.getInstance().getConnection();
+        String sql = """
+                INSERT INTO MmrBalance(
+                    first_player_uuid,
+                    second_player_uuid,
+                    created,
+                    updated,
+                    first_player_balance,
+                    second_player_balance
+                ) VALUES (?, ?, ?, ?, ?, ?);
+                """;
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            LocalDateTime today = LocalDateTime.now();
+            Logger.getInstance().info(today.toString());
+            stmt.setString(1, mmrBalanceEntity.firstPlayerUUID().toString());
+            stmt.setString(2, mmrBalanceEntity.secondPlayerUUID().toString());
+            stmt.setString(3, today.toString());
+            stmt.setString(4, today.toString());
+            stmt.setInt(5, 0);
+            stmt.setInt(6, 0);
+            Logger.getInstance().info(stmt.toString());
             stmt.executeUpdate();
         } catch (SQLException e) {
             Logger.getInstance().error(e.getMessage());
